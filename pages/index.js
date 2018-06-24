6a76c5cf569c1b3f6ef5e738a73891471b51f381
wheredoesyourmindgo <moemanchu@icloud.com>
2018-06-24 09:47:42 -0700

    First commit.

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+// @flow
+import * as React from 'react';
+
+const index = () => (
+  <div>
+    <h1>My Mapp</h1>
+    <style jsx>{`
+      h1,
+      a {
+        font-family: 'Arial';
+      }
+
+      ul {
+        padding: 0;
+      }
+    `}</style>
+  </div>
+);
+
+export default index;
