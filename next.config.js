bf43140af2dd7f7fc10e20ce2a3158dc5cd1202b
wheredoesyourmindgo <moemanchu@icloud.com>
2018-06-24 17:42:17 -0700

    Analyze fixed and extended.

--- next.config.js
+++ next.config.js
@@ -1,15 +1,19 @@
 const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer')
+const {
+  WebpackBundleSizeAnalyzerPlugin
+} = require('webpack-bundle-size-analyzer')
 const withCSS = require('@zeit/next-css')
 const {ANALYZE} = process.env
 
 // withCSS allows for CSS module imports using next-css.
 module.exports = withCSS({
-  webpack: function(config) {
+  webpack: function(config, {isServer}) {
     if (ANALYZE) {
       config.plugins.push(
+        new WebpackBundleSizeAnalyzerPlugin('stats.txt'),
         new BundleAnalyzerPlugin({
           analyzerMode: 'server',
-          analyzerPort: 8888,
+          analyzerPort: isServer ? 8888 : 8889,
           openAnalyzer: true
         })
       )
