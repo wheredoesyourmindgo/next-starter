421b3a5536e0659d9ec52d2b37f0510bcde05e69
wheredoesyourmindgo <moemanchu@icloud.com>
2018-06-24 17:48:26 -0700

    Stats and analyze are now separate npm scripts.

--- next.config.js
+++ next.config.js
@@ -3,20 +3,23 @@ const {
   WebpackBundleSizeAnalyzerPlugin
 } = require('webpack-bundle-size-analyzer')
 const withCSS = require('@zeit/next-css')
-const {ANALYZE} = process.env
+const {ANALYZE, STATS} = process.env
 
 // withCSS allows for CSS module imports using next-css.
 module.exports = withCSS({
   webpack: function(config, {isServer}) {
     if (ANALYZE) {
       config.plugins.push(
-        new WebpackBundleSizeAnalyzerPlugin('stats.txt'),
         new BundleAnalyzerPlugin({
           analyzerMode: 'server',
           analyzerPort: isServer ? 8888 : 8889,
           openAnalyzer: true
-        })
+        }),
+        new WebpackBundleSizeAnalyzerPlugin('stats.txt')
       )
+    }
+    if (STATS) {
+      config.plugins.push(new WebpackBundleSizeAnalyzerPlugin('stats.txt'))
     }
 
     return config
