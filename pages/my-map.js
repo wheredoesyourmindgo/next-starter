36ba1aa9f19acb26facfe352aff1ab0deca2011e
wheredoesyourmindgo <moemanchu@icloud.com>
2018-07-02 19:36:36 -0700

    Better width and height handling.

--- pages/my-map.js
+++ pages/my-map.js
@@ -3,6 +3,10 @@ import * as React from 'react'
 import ReactMapGL, {NavigationControl} from 'react-map-gl'
 import Layout from '../components/layout'
 import 'mapbox-gl/dist/mapbox-gl.css'
+import {debounce} from 'lodash'
+
+// Layout Header and Footer are 27px in height.
+const SUBTRACT_FROM_HEIGHT = 54
 
 type Props = {
   windowWidth: number,
@@ -22,6 +26,9 @@ type State = {
 }
 
 class MyMap extends React.Component<Props, State> {
+  /**
+   * window may not be available in getInitialProps. try/catch block used to prevent runtime error.
+   */
   static async getInitialProps() {
     try {
       return {windowWidth: window.innerWidth, windowHeight: window.innerHeight}
@@ -44,19 +51,38 @@ class MyMap extends React.Component<Props, State> {
           viewport: {
             ...prevState.viewport,
             width: window.innerWidth,
-            height: window.innerHeight
+            height: window.innerHeight - SUBTRACT_FROM_HEIGHT
           }
         }
       })
     } catch (err) {
       console.warn('Error during componentDidMount.')
     }
+
+    /**
+     * Resize map during window resize event.
+     */
+    window.addEventListener(
+      'resize',
+      debounce(() => {
+        this.setState((prevState) => {
+          return {
+            viewport: {
+              ...prevState.viewport,
+              width: window.innerWidth,
+              height: window.innerHeight - SUBTRACT_FROM_HEIGHT
+            }
+          }
+        })
+      }),
+      200
+    )
   }
 
   state = {
     viewport: {
-      width: this.props.windowWidth || 500,
-      height: this.props.windowHeight || 500,
+      width: this.props.windowWidth || 1000,
+      height: this.props.windowHeight - SUBTRACT_FROM_HEIGHT || 1000,
       latitude: 37.7577,
       longitude: -122.4376,
       zoom: 8
@@ -75,8 +101,8 @@ class MyMap extends React.Component<Props, State> {
     // const {viewport, updateViewport} = this.props
 
     return (
-      <Layout title="Map">
-        <div>
+      <div>
+        <Layout title="Map">
           <ReactMapGL
             mapboxApiAccessToken={process.env._MAPBOX_ACCESS_TOKEN_}
             {...this.state.viewport}
@@ -87,8 +113,8 @@ class MyMap extends React.Component<Props, State> {
               <NavigationControl onViewportChange={this.onUpdateViewport} />
             </div>
           </ReactMapGL>
-        </div>
-      </Layout>
+        </Layout>
+      </div>
     )
   }
 }
